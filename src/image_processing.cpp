754e4c0fbacda5229201c05db3115a87660cf38e
root <root@DESKTOP-6KMCL72>
2024-12-17 20:36:10 +0100

    add functinon which setup grid place all images on it and returns it by value

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -1,5 +1,7 @@
-// header
+// local
 #include "image_processing.hpp"
+#include "grid.hpp"
+#include "stroke.hpp"
 
 // std
 #include <fstream>
@@ -60,4 +62,51 @@ cv::Mat CropImageToSymbol(const cv::Mat &input_mat) {
   }
   return input_mat(bounding_box);
 }
+
+Grid PlceOnGrid(const std::vector<cv::Mat> &grayscale_images,
+                const std::vector<cv::Point2f> &images_positions) {
+
+  // create vector of strokes
+  std::vector<mathboard::Stroke> strokes;
+  strokes.resize(grayscale_images.size());
+  for (std::size_t i = 0; i < strokes.size(); i++) {
+    strokes[i] = Stroke(i, images_positions[i], grayscale_images[i]);
+  }
+
+  // calculate boundaries of grid
+  cv::Point2f bot_right_corner{0, 0};
+  cv::Point2f top_left_corner{INFINITY, INFINITY};
+  for (std::size_t i = 0; i < strokes.size(); i++) {
+    const cv::Point2f line_pos = strokes[i].GetPosition();
+    const cv::Rect line_BB = strokes[i].GetBoundingBox();
+
+    if (bot_right_corner.x < line_pos.x + line_BB.width) {
+      bot_right_corner.x = line_pos.x + line_BB.width;
+    }
+    if (top_left_corner.x > line_pos.x) {
+      top_left_corner.x = line_pos.x;
+    }
+    if (bot_right_corner.y < line_pos.y + line_BB.height) {
+      bot_right_corner.y = line_pos.y + line_BB.height;
+    }
+    if (top_left_corner.y > line_pos.y) {
+      top_left_corner.y = line_pos.y;
+    }
+  }
+
+  // calculate avrage size of one image
+  cv::Size2f avrage_size_of_image{0.0f, 0.0f};
+  for (std::size_t i = 0; i < strokes.size(); i++) {
+    avrage_size_of_image.width += strokes[i].GetBoundingBox().size().width;
+    avrage_size_of_image.height += strokes[i].GetBoundingBox().size().height;
+  }
+  avrage_size_of_image.width /= static_cast<float>(strokes.size());
+  avrage_size_of_image.height /= static_cast<float>(strokes.size());
+
+  // create grid
+  Grid grid(top_left_corner, bot_right_corner, avrage_size_of_image);
+  grid.InsertObjects(strokes);
+  return grid;
+}
+
 } // namespace mathboard
