916eb8381abe515d6ec5c2dd67ea884868d9c722
Mikołaj Lubiak <lubiak@proton.me>
2025-01-19 19:56:09 +0100

    Add daemon

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,4 @@
 // local
-#include "helper/solve_equation.hpp"
+#include "daemon.hpp"
 
-// libs
-// spdlog
-#include <spdlog/spdlog.h>
-
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    spdlog::error("[main]: Usage: {} <image_path>", argv[0]);
-    return EXIT_FAILURE;
-  }
-
-  const std::string imagePath = argv[1];
-
-  mathboard::solveImage(imagePath);
-
-  return EXIT_SUCCESS;
-}
+int main(int argc, char **argv) { mathboard::Daemon(); }
