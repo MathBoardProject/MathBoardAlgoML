be7ec4b506f2cecddff3a75b2e00caa0b962df79
root <root@DESKTOP-6KMCL72>
2025-03-10 18:54:44 +0100

    feat(python): provide TensorFlow dataset of basic handwritten math symbols dataset
    Create BHMSDS as a `tfds` dataset builder inheriting from `tfds.core.GeneratorBasedBuilder`.
    Download the dataset using the Kaggle API.
    The BHMSDS can be used like any other built-in `tfds` dataset. It has two default splits: `train` and `test`, so using it is pretty straightforward.
    
    See the dataset details at: https://www.kaggle.com/datasets/c0wie08/basic-handwritten-math-symbols-dataset

---
+++ src/py/bhmsds/bhmsds_dataset_builder_test.py
@@ -0,0 +1,25 @@
+"""bhmsds dataset."""
+
+from . import bhmsds_dataset_builder
+import tensorflow_datasets as tfds
+
+
+class BhmsdsTest(tfds.testing.DatasetBuilderTestCase):
+    """Tests for bhmsds dataset."""
+
+    # TODO(bhmsds):
+    DATASET_CLASS = bhmsds_dataset_builder.Builder
+    SPLITS = {
+        "train": 3,  # Number of fake train example
+        "test": 1,  # Number of fake test example
+    }
+
+    # If you are calling `download/download_and_extract` with a dict, like:
+    #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
+    # then the tests needs to provide the fake output paths relative to the
+    # fake data directory
+    # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+
+
+if __name__ == "__main__":
+    tfds.testing.test_main()
